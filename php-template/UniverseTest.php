7d675e630896f5ef1224f1a0561304d94ec5bbfd
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-31 22:54:51 +0100

    Update UniverseTest.php

--- php-template/UniverseTest.php
+++ php-template/UniverseTest.php
@@ -1,6 +1,6 @@
 <div id='tmp'></div>
 <?php
-	$html = str_replace("\n","",file_get_contents('fichier.txt'));
+	$html = str_replace("\n","",file_get_contents('Universe.php'));
 	$html = str_replace("\r","",$html);
 	echo "<script>";
 	echo "document.getElementById('tmp').innerhtml ='$html';";
