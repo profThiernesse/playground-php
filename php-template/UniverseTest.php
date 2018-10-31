4540d093198b2f3c21ce9c03a7b8a4f01f9b8b4e
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-31 22:36:44 +0100

    Update UniverseTest.php

--- php-template/UniverseTest.php
+++ php-template/UniverseTest.php
@@ -3,6 +3,7 @@
 	$html = file_get_contents('Universe.php');
 	echo "<script>";
 	echo "document.getelementbyid('tmp').innerhtml ='" . $html . "'";
+	echo "console.log('$html')";
 	echo "</script>";
 	echo $html;
 ?>
