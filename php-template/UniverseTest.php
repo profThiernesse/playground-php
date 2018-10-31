917a476d5aa9e5a126f3b1274980926957eaa96e
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-31 22:39:08 +0100

    Update UniverseTest.php

--- php-template/UniverseTest.php
+++ php-template/UniverseTest.php
@@ -1,6 +1,6 @@
 <div id='tmp'>dd</div>
 <?php
-	$html = file_get_contents('Universe.php');
+	$html = str_replace(PHP_EOL,"",file_get_contents('Universe.php'));
 	echo "<script>";
 	echo "document.getelementbyid('tmp').innerhtml ='" . $html . "'";
 	echo "console.log('$html')";
