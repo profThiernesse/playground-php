56d10e08f3fec1b8c2b9478438d31138bfeadefc
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-31 21:36:52 +0100

    Update run.sh

--- php-template/run.sh
+++ php-template/run.sh
@@ -1,3 +1,4 @@
 php UniverseTest.php > UniverseTest.html
-echo "TECHIO> open -s /project/target/ UniverseTest.html"
 echo "TECHIO> message Mon message"
+echo "TECHIO> open -s /project/target/ UniverseTest.html"
+
