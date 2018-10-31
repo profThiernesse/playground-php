f524ed2f3ffe0088755cbe26a30983924bb43c03
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-31 23:05:06 +0100

    Update UniverseTest.php

--- php-template/UniverseTest.php
+++ php-template/UniverseTest.php
@@ -1,4 +1,4 @@
-<div id='tmp'></div>
+echo $html;
 <?php
 	$html = str_replace("\n","",file_get_contents('Universe.php'));
 	$html = str_replace("\r","",$html);
@@ -7,6 +7,7 @@
 	echo "el.innerHTML ='$html';";
 	echo "console.log(el);";
 	echo "console.log(el.getElementsByTagName('p'));";
+	echo "TECHIO> message PROUT";
 	echo "</script>";
-	echo $html;
+	
 ?>
