73ba58e199f4aedce7e2afed41b41f06eecf9f33
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-31 22:57:45 +0100

    Update UniverseTest.php

--- php-template/UniverseTest.php
+++ php-template/UniverseTest.php
@@ -3,8 +3,9 @@
 	$html = str_replace("\n","",file_get_contents('Universe.php'));
 	$html = str_replace("\r","",$html);
 	echo "<script>";
-	echo "document.getElementById('tmp').innerhtml ='$html';";
-	echo "console.log('$html');";
+	echi "var el = document.createElement( 'html' );";
+	echo "el.innerhtml ='$html';";
+	echo "console.log(el.getElementsByTagName( 'p' ));";
 	echo "</script>";
 	echo $html;
 ?>
