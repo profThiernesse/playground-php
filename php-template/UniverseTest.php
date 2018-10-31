355a2808287caa7c713000eb02ce1dc0a065144a
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-31 23:18:31 +0100

    Update UniverseTest.php

--- php-template/UniverseTest.php
+++ php-template/UniverseTest.php
@@ -6,7 +6,7 @@
 	echo "el.innerHTML ='$html';";
 	echo "console.log(el);";
 	echo "console.log(el.getElementsByTagName('p'));";
-	echo "TECHIO> message salut";
+	echo "\nTECHIO> message salut\n";
 	echo "</script>";
 	
 ?>
