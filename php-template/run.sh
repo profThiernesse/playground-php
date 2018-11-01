2cf7d711a5ce999805f6bc08851bd21fbfbda727
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-11-01 13:13:36 +0100

    Update php-template/run.sh

--- php-template/run.sh
+++ php-template/run.sh
@@ -4,7 +4,8 @@ php $1".php" > "output.html"
 
 
 cat output.html
-ls -al
-ls .. -al
+ls ../target -al
+ls ../workspace -al
+
 php Test.php "output.html" $2
 echo "TECHIO> open -s /project/target/ output.html"
