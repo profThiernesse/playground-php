fa421eb1f376d3378448ebbe787d96647c151f0f
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-11-01 12:31:08 +0100

    Update php-template/run.sh

--- php-template/run.sh
+++ php-template/run.sh
@@ -1,4 +1,5 @@
-echo "TECHIO> open -s /project/target/ $1.html"
+
 echo "TECHIO> redirect-streams 'Output'"
 php $1".php" > "output.html"
 php Test.php "output.html" $2
+echo "TECHIO> open -s /project/target/ output.html"
