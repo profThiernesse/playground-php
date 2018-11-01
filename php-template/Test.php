880cb9dd4311b1d826d24aa1da7d5d36f7a0707e
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-11-01 13:26:52 +0100

    Update php-template/Test.php

--- php-template/Test.php
+++ php-template/Test.php
@@ -12,7 +12,7 @@
 	
 	function check_DOM($file,$exp) {
 		// $html = file_get_html($file);
-		$html = str_get_html(file_get_contents($argv[1]));
+		$html = str_get_html(file_get_contents($file));
 		$dom_string = go_DOM($html->root);
 		
 		$dom_string = str_replace("comment--","",$dom_string);
