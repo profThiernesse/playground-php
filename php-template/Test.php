b83a2376b467b641df55fe4233d9da8b03c82ad9
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-11-01 12:08:05 +0100

    Update php-template/Test.php

--- php-template/Test.php
+++ php-template/Test.php
@@ -1,3 +1,50 @@
 <?php
+
+	include("simple_html_dom.php");
+	$exp = "^h1--div--([.*ul--[li-id-(li-.*-){3,}].*])$";
+	check_DOM($argv[1],$exp);
+	
 	var_dump($argv);
+	
+	function check_DOM($file,$exp) {
+		$html = file_get_html($file);
+		$dom_string = go_DOM($html->root);
+		$exp = str_replace(array("[","]"),array("\[","\]"),$exp);
+		
+		if (preg_match("#$exp#",$dom_string)) {
+			echo "TECHIO> succes true\n";
+		}
+		else {
+			echo "TECHIO> succes false\n";
+			echo "TECHIO> message --channel 'Error' 'DOM failed'\n";
+		}
+	}
+	
+	function go_DOM($html) {
+		
+		$retval ="";
+		foreach($html->children as $children) {
+			$retval .= $children->tag . element_attr($children) ;
+			if (!empty($children->children)) $retval .= "[" . go_DOM($children) . "]";
+		}
+		return $retval;
+	}
+	
+	function element_attr($tag) {
+		$retval="-";
+		$attrs = $tag->attr;
+		ksort($attrs);
+		foreach($attrs as $attr => $value)
+			$retval.=$attr;
+		$retval.="-";
+		return $retval;
+	}
+	
+	function require_tag($html,$tag) {
+		$element = $html->find($tag);
+		if (empty($element)) {
+			echo "TECHIO> message --channel 'Error' '$tag tag not found.'\n";
+		}
+		return $element;
+	}	
 ?>
